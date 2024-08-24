c9c4f14665da8222d74e29dd9584413497752add
Sofia <sofia.sarotri24@gmail.com>
2024-08-23 22:35:49 -0300

    new headders

--- web_scraping_hltv.py
+++ web_scraping_hltv.py
@@ -12,10 +12,12 @@ class Web_scrapper():
         self.url_for_lineup = 'https://www.hltv.org/team/8297/furia#tab-rosterBox'
         self.url_for_player_stats = 'https://www.hltv.org/stats/players/'
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive'
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Referer": "https://www.google.com/",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
         }
 
     def get_upcoming_events(self):
